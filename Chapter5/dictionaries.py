cb4c7071e6b1ac03c7b7e35d60db1d42d7b72452
Andy Kloc <andykloc@Andys-MacBook-Pro.local>
2020-03-04 19:57:20 -0500

    Up to "The setdefault() method" in chapter 5.

---
+++ Chapter5/dictionaries.py
@@ -0,0 +1,50 @@
+# Dictionaries, just using strings.
+myCat = {'size': 'fat', 'color': 'gray', 'disposition':'loud'}
+print('My cat has ' + myCat['color'] + ' fur.')
+
+# Dictionaries can  use different types for keys or values.
+luggage = {'color': 'black', 'combination': 12345}
+luggage2 = {'color': 'brown', 12345: 'combination'}
+print('My luggage is ' + luggage['color'] + ' and its combo is ' + str(luggage['combination']))
+print('Your luggage is ' + luggage2['color'] + ' and 12345 is its ' + luggage2[12345])
+
+# Main differences between dictionaries and lists:
+#  - dictionaries aren't ordered (3.7 supports this with some extra work)
+#  - dictionaries can't be sliced.
+
+# You can check for keys being present like so:
+birthdays = {'Alice': 'Apr 1', 'Bob': 'Dec 12', 'Carol': 'Mar 4'}
+if 'Alice' in birthdays:
+    print('Alices birthday is ' + birthdays['Alice'])
+
+# keys(), values() and items() methods
+
+# values() pulls back all of the values of a dictionary.
+spam = {'color': 'red', 'age':42}
+for v in spam.values():
+    print(v)
+# keys pulls back all of the keys in the dictionary.
+for k in spam.keys():
+    print(k)
+
+# items pulls back key AND value.
+for i in spam.items():
+    print(i)
+
+# Getting fancier and pulling back keys and values separately...
+for k, v in spam.items():
+    print('Key is ' + k + ', value is ' + str(v))
+
+# Does a key exist in the dictionary?
+if 'color' in spam.keys():
+    print('color is a key in spam.')
+# Does a value exist in the dictionary?
+if 42 in spam.values():
+    print('42 is one of the values.')
+
+# What if we want to grab a value but don't know if the key is in there?
+# Instead of checking using the "in" keyword, just use "get" and it'll perform the check.
+picnicItems = {'apples': 5, 'cups': 2}
+print('I am bringing ' + str(picnicItems.get('eggs', 0)) + ' eggs.')
+
+# LEFT OFF AT "The setdefault() method"
